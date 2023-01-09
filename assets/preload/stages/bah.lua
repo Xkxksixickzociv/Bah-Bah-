af8ba2e50abecf2a38e6f1c590459d88bed3d84f
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-09 14:47:31 +0200

    Add files via upload

---
+++ assets/preload/stages/bah.lua
@@ -0,0 +1,10 @@
+function onCreate()
+
+  makeLuaSprite('bah', 'bah', -400, -300);
+  addLuaSprite('bah', false);
+  setLuaSpriteScrollFactor('bah', 0.9, 0.9);
+	scaleObject('bah', 1.2, 1.4);
+
+close(true);
+end
+
